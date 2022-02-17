5f829a15acfbe58c84381193f5cdc9336e2e9fa0
Ramdas Hedgapure <hedgapurer@gmail.com>
2022-02-17 20:02:44 +0530

    added comments

--- authentication/src/models/user.js
+++ authentication/src/models/user.js
@@ -1,34 +1,40 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcryptjs')
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({
-    email : {type : String, required : true, unique : true},
-    password : {type : String, required : true},
-},{
-    versionKey : false
-})
+const userSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  {
+    versionKey: false,
+  }
+);
 
-userSchema.pre('save', function (next){  //for create, update
-    if(!this.isModified('password')) return next();
+//Hash before saving password
+userSchema.pre("save", function (next) {
+  //for create, update
+  if (!this.isModified("password")) return next();
 
-    // const hash = bcrypt.hashSync(this.password, 10)
-    // this.password = hash
-    // return next()
-    
-    //-------OR------
-    bcrypt.hash(this.password, 10, (err, hash)=>{
-        this.password= hash;
-        return next()
-    })
-})
+  // const hash = bcrypt.hashSync(this.password, 10)
+  // this.password = hash
+  // return next()
 
-userSchema.methods.checkPassword = function (password){
-    return new Promise((resolve, reject)=>{
-        bcrypt.compare(password, this.password, function (err, same){
-            if (err) return reject(err)
-            return resolve(same)
-        })
-    })
-}
+  //-------OR------
+  bcrypt.hash(this.password, 10, (err, hash) => {
+    this.password = hash;
+    return next();
+  });
+});
 
-module.exports = mongoose.model('user', userSchema)
+//Check password while login
+userSchema.methods.checkPassword = function (password) {
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, this.password, function (err, same) {
+      if (err) return reject(err);
+      return resolve(same);
+    });
+  });
+};
+
+module.exports = mongoose.model("user", userSchema);
--- authentication/src/server.js
+++ authentication/src/server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-const connect = require('./configs/db')
-const {register, login} = require('./controllers/auth')
+const connect = require("./configs/db");
+const { register, login } = require("./controllers/auth");
 
-app.post('/register', register)
-app.post('/login', login)
+app.post("/register", register);
+app.post("/login", login);
 
-app.listen(2000, async ()=>{
-    await connect()
-    console.log('listening on 2000')
-})
+app.listen(2000, async () => {
+  await connect();
+  console.log("listening on 2000");
+});
