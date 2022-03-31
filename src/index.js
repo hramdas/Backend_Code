65f807f16a02d55e0ac179019e04914f3e63ad26
Ramdas Hedgapure <hedgapurer@gmail.com>
2022-03-31 17:12:39 +0530

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-app.use(express.json())
-
-app.get('/',(req, res)=>{
-    res.status(200).send("Server working")
-})
-
-module.exports = app
